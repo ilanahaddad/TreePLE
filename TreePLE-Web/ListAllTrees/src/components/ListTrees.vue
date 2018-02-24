c565aedab60ed578bcf0d2e4df7ec91987b09817
Thomas Hannaford <thomas.hannaford@mail.mcgill.ca>
2018-02-24 16:22:29 -0500

    Web changes - still no create tree functionality
    Web changes - still no create tree functionality

--- TreePLE-Web/ListAllTrees/src/components/ListTrees.vue
+++ TreePLE-Web/ListAllTrees/src/components/ListTrees.vue
@@ -5,24 +5,10 @@
       <tr>
           <td>ID</td>
           <td>Species Type</td>
+	  <td>Latitude</td>
+	  <td>Longitude</td>
 	  <td>Status</td>
       </tr>
-      <tr>
-          <td>
-              <input type="text" placeholder="Species">
-          </td>
-	  <td>
-              <input type="text" placeholder="Latitude">
-          </td>
-<td>
-              <input type="text" placeholder="Longitude">
-          </td>
-          <td>
-              <button>Create tree</button>
-          </td>
-      </tr>
-    </table>
-    <table>
       <tr v-for="tree in trees" >
         <td>{{ tree.id }}</td>
         <td>{{ tree.species }}</td>
@@ -31,6 +17,23 @@
         <td>{{ tree.status }}</td>
       </tr>
     <!-- ... -->
+      <tr>
+          <td>
+              <input type="text" placeholder="Species">
+          </td>
+	  <td>
+              <input type="text" placeholder="Latitude">
+          </td>
+          <td>
+              <input type="text" placeholder="Longitude">
+          </td>
+          <td>
+              <input type="text" placeholder="Status">
+          </td>
+          <td>
+              <button>Create tree</button>
+          </td>
+      </tr>
     </table>
     <p>
       <span style="color:red">Error: Message text comes here</span>
