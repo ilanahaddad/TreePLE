f3a381f3550f1b72b63e94888b0369e672bacd15
asma-alromaih <asma.alromaih@mail.mcgill.ca>
2018-03-17 14:34:14 -0400

    Added calculations for sustainability report

--- TreePLE/src/main/java/ca/mcgill/ecse321/TreePLE/model/SustainabilityReport.java
+++ TreePLE/src/main/java/ca/mcgill/ecse321/TreePLE/model/SustainabilityReport.java
@@ -9,142 +9,163 @@ import java.util.*;
 public class SustainabilityReport
 {
 
-  //------------------------
-  // MEMBER VARIABLES
-  //------------------------
+	//------------------------
+	// MEMBER VARIABLES
+	//------------------------
 
-  //SustainabilityReport Attributes
-  private String reporterName;
-  private Date date;
+	//SustainabilityReport Attributes
+	private String reporterName;
+	private Date date;
 
-  //SustainabilityReport Associations
-  private List<Location> reportPerimeter;
+	//SustainabilityReport Associations
+	private List<Location> reportPerimeter;
 
-  //------------------------
-  // CONSTRUCTOR
-  //------------------------
+	//------------------------
+	// CONSTRUCTOR
+	//------------------------
 
-  public SustainabilityReport(String aReporterName, Date aDate, Location... allReportPerimeter)
-  {
-    reporterName = aReporterName;
-    date = aDate;
-    reportPerimeter = new ArrayList<Location>();
-    boolean didAddReportPerimeter = setReportPerimeter(allReportPerimeter);
-    if (!didAddReportPerimeter)
-    {
-      throw new RuntimeException("Unable to create SustainabilityReport, must have 4 reportPerimeter");
-    }
-  }
+	public SustainabilityReport(String aReporterName, Date aDate, Location... allReportPerimeter)
+	{
+		reporterName = aReporterName;
+		date = aDate;
+		reportPerimeter = new ArrayList<Location>();
+		boolean didAddReportPerimeter = setReportPerimeter(allReportPerimeter);
+		if (!didAddReportPerimeter)
+		{
+			throw new RuntimeException("Unable to create SustainabilityReport, must have 4 reportPerimeter");
+		}
+	}
 
-  //------------------------
-  // INTERFACE
-  //------------------------
+	//------------------------
+	// INTERFACE
+	//------------------------
 
-  public boolean setReporterName(String aReporterName)
-  {
-    boolean wasSet = false;
-    reporterName = aReporterName;
-    wasSet = true;
-    return wasSet;
-  }
+	public boolean setReporterName(String aReporterName)
+	{
+		boolean wasSet = false;
+		reporterName = aReporterName;
+		wasSet = true;
+		return wasSet;
+	}
 
-  public boolean setDate(Date aDate)
-  {
-    boolean wasSet = false;
-    date = aDate;
-    wasSet = true;
-    return wasSet;
-  }
+	public boolean setDate(Date aDate)
+	{
+		boolean wasSet = false;
+		date = aDate;
+		wasSet = true;
+		return wasSet;
+	}
 
-  public String getReporterName()
-  {
-    return reporterName;
-  }
+	public String getReporterName()
+	{
+		return reporterName;
+	}
 
-  public Date getDate()
-  {
-    return date;
-  }
+	public Date getDate()
+	{
+		return date;
+	}
 
-  public Location getReportPerimeter(int index)
-  {
-    Location aReportPerimeter = reportPerimeter.get(index);
-    return aReportPerimeter;
-  }
+	public Location getReportPerimeter(int index)
+	{
+		Location aReportPerimeter = reportPerimeter.get(index);
+		return aReportPerimeter;
+	}
 
-  public List<Location> getReportPerimeter()
-  {
-    List<Location> newReportPerimeter = Collections.unmodifiableList(reportPerimeter);
-    return newReportPerimeter;
-  }
+	public List<Location> getReportPerimeter()
+	{
+		List<Location> newReportPerimeter = Collections.unmodifiableList(reportPerimeter);
+		return newReportPerimeter;
+	}
 
-  public int numberOfReportPerimeter()
-  {
-    int number = reportPerimeter.size();
-    return number;
-  }
+	public int numberOfReportPerimeter()
+	{
+		int number = reportPerimeter.size();
+		return number;
+	}
 
-  public boolean hasReportPerimeter()
-  {
-    boolean has = reportPerimeter.size() > 0;
-    return has;
-  }
+	public boolean hasReportPerimeter()
+	{
+		boolean has = reportPerimeter.size() > 0;
+		return has;
+	}
 
-  public int indexOfReportPerimeter(Location aReportPerimeter)
-  {
-    int index = reportPerimeter.indexOf(aReportPerimeter);
-    return index;
-  }
+	public int indexOfReportPerimeter(Location aReportPerimeter)
+	{
+		int index = reportPerimeter.indexOf(aReportPerimeter);
+		return index;
+	}
 
-  public static int requiredNumberOfReportPerimeter()
-  {
-    return 4;
-  }
+	public static int requiredNumberOfReportPerimeter()
+	{
+		return 4;
+	}
 
-  public static int minimumNumberOfReportPerimeter()
-  {
-    return 4;
-  }
+	public static int minimumNumberOfReportPerimeter()
+	{
+		return 4;
+	}
 
-  public static int maximumNumberOfReportPerimeter()
-  {
-    return 4;
-  }
+	public static int maximumNumberOfReportPerimeter()
+	{
+		return 4;
+	}
 
-  public boolean setReportPerimeter(Location... newReportPerimeter)
-  {
-    boolean wasSet = false;
-    ArrayList<Location> verifiedReportPerimeter = new ArrayList<Location>();
-    for (Location aReportPerimeter : newReportPerimeter)
-    {
-      if (verifiedReportPerimeter.contains(aReportPerimeter))
-      {
-        continue;
-      }
-      verifiedReportPerimeter.add(aReportPerimeter);
-    }
+	public boolean setReportPerimeter(Location... newReportPerimeter)
+	{
+		boolean wasSet = false;
+		ArrayList<Location> verifiedReportPerimeter = new ArrayList<Location>();
+		for (Location aReportPerimeter : newReportPerimeter)
+		{
+			if (verifiedReportPerimeter.contains(aReportPerimeter))
+			{
+				continue;
+			}
+			verifiedReportPerimeter.add(aReportPerimeter);
+		}
 
-    if (verifiedReportPerimeter.size() != newReportPerimeter.length || verifiedReportPerimeter.size() != requiredNumberOfReportPerimeter())
-    {
-      return wasSet;
-    }
+		if (verifiedReportPerimeter.size() != newReportPerimeter.length || verifiedReportPerimeter.size() != requiredNumberOfReportPerimeter())
+		{
+			return wasSet;
+		}
 
-    reportPerimeter.clear();
-    reportPerimeter.addAll(verifiedReportPerimeter);
-    wasSet = true;
-    return wasSet;
-  }
+		reportPerimeter.clear();
+		reportPerimeter.addAll(verifiedReportPerimeter);
+		wasSet = true;
+		return wasSet;
+	}
 
-  public void delete()
-  {
-    reportPerimeter.clear();
-  }
+	public void delete()
+	{
+		reportPerimeter.clear();
+	}
 
 
-  public String toString()
-  {
-    return super.toString() + "["+
-            "reporterName" + ":" + getReporterName()+ "]" + System.getProperties().getProperty("line.separator") +
-            "  " + "date" + "=" + (getDate() != null ? !getDate().equals(this)  ? getDate().toString().replaceAll("  ","    ") : "this" : "null");
-  }
+	public String toString()
+	{
+		return super.toString() + "["+
+				"reporterName" + ":" + getReporterName()+ "]" + System.getProperties().getProperty("line.separator") +
+				"  " + "date" + "=" + (getDate() != null ? !getDate().equals(this)  ? getDate().toString().replaceAll("  ","    ") : "this" : "null");
+	}
+
+	public double[] calculateSustainabilityAttributes(int numTrees, int numSpecies, int crownDiameter, int height, int width, int age) {
+		double biodiversityIndex = numSpecies/numTrees;
+		double canopy = 2*Math.PI*Math.pow(crownDiameter, 2);
+
+		double weight;
+		if(width < 11) {
+			weight = 0.25*Math.pow(width,2)*height;
+		}
+		else {
+			weight = 0.15*Math.pow(width,2)*height;
+		}
+		double dryWeight = 0.725 * weight;
+		double carbonWeight = 0.5*dryWeight;
+		double  CO2Weight = carbonWeight*3.6663;
+		double carbonSequestration = CO2Weight/age;
+
+		double [] sustainabilityAttributes = {biodiversityIndex, canopy, carbonSequestration};
+
+		return sustainabilityAttributes;
+	}
 }
