f510322a7500909e9609bf46278fa62e13e7c219
ilana-haddad <ilana.haddad@mail.mcgill.ca>
2018-03-27 15:18:33 -0400

    set user type first attempt for android and setUserType first attempt

--- TreePLE-Android/app/src/main/java/ca/mcgill/ecse321/treeple_android/MainActivity.java
+++ TreePLE-Android/app/src/main/java/ca/mcgill/ecse321/treeple_android/MainActivity.java
@@ -4,12 +4,38 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+    private String error = null;
+    //Added from online:
+    /*
+    private Spinner userTypeSpinner;
+    private Button btnSetUserType; */
+    //Added from tutorial:
+    private List<String> userTypes = new ArrayList<>();
+    private ArrayAdapter<String> userTypeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +52,19 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        //Added from online:
+        /*
+        addListenerOnButton();
+        addListenerOnSpinnerItemSelection();*/
+        //Added from tutorial:
+        Spinner userTypeSpinner = (Spinner) findViewById(R.id.userTypeSpinner);
+        userTypeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, userTypes);
+        userTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        userTypeSpinner.setAdapter(userTypeAdapter);
+        //Get initial contents for spinners:
+        refreshLists(this.getCurrentFocus());
+        // initialize error message text view
+        refreshErrorMessage();
     }
 
     @Override
@@ -49,4 +88,134 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+
+    }
+    public void refreshLists(View view) {
+        refreshList(userTypeAdapter ,userTypes, "setUserType");
+    }
+
+    private void refreshList(final ArrayAdapter<String> adapter, final List<String> names, String restFunctionName) {
+        HttpUtils.get(restFunctionName, new RequestParams(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                names.clear();
+                names.add("Please select...");
+                for( int i = 0; i < response.length(); i++){
+                    try {
+                        names.add(response.getJSONObject(i).getString("name"));
+                    } catch (Exception e) {
+                        error += e.getMessage();
+                    }
+                    refreshErrorMessage();
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+    }
+    public void setUserType(View v) {
+        Spinner userTypeSpin = (Spinner) findViewById(R.id.userTypeSpinner);
+        error = "";
+
+        //Issue and HTTP POST
+        RequestParams rp = new RequestParams();
+        rp.add("userType", userTypeSpin.getSelectedItem().toString());
+
+        HttpUtils.post("setUserType/" , rp, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage();
+                ((Spinner) findViewById(R.id.userTypeSpinner)).getSelectedItem();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+        // Set back the spinners to the initial state after posting the request
+        userTypeSpin.setSelection(0);
+        refreshErrorMessage();
+    }
+    /*
+    public void addListenerOnSpinnerItemSelection(){
+        userTypeSpinner = (Spinner) findViewById(R.id.userTypeSpinner);
+        userTypeSpinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+    //get the selected dropdown list value
+    public void addListenerOnButton(){
+        userTypeSpinner = (Spinner) findViewById(R.id.userTypeSpinner);
+        btnSetUserType = (Button) findViewById(R.id.btnSetUserType);
+        btnSetUserType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner 1 : "+ String.valueOf(userTypeSpinner.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        HttpUtils.post("setUserType/" + String.valueOf(userTypeSpinner.getSelectedItem(), new RequestParams(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage();
+                //tv.setText("");
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+    }
+
+    public void setUserType(View v) {
+        error = "";
+        final TextView tv = (TextView) findViewById(R.id.userTypeSpinner);
+        HttpUtils.post("setUserType/" + tv.getText().toString(), new RequestParams(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                refreshErrorMessage();
+                tv.setText("");
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+    }*/
 }
