f9e88bb16f22cb8f5ef0673aafb0d694fec88df5
asma-alromaih <asma.alromaih@mail.mcgill.ca>
2018-04-02 15:53:39 -0400

    created buttons for survey page

--- TreePLE-Android/app/src/main/java/ca/mcgill/ecse321/treeple_android/OptionsActivity.java
+++ TreePLE-Android/app/src/main/java/ca/mcgill/ecse321/treeple_android/OptionsActivity.java
@@ -2,16 +2,23 @@ package ca.mcgill.ecse321.treeple_android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by ilanahaddad on 2018-03-27.
  */
 
 public class OptionsActivity extends Activity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Get the view from activity_options.xml
         setContentView(R.layout.activity_options);
+     }
+
+    public void viewSurveysPage(View view) {
+
     }
 }
