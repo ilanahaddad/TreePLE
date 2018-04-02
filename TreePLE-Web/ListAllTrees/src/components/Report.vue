028dd3e6ba0f9f64ecd28a6612bfa37e069f1ad5
Thomas Hannaford <thomas.hannaford@mail.mcgill.ca>
2018-04-02 17:45:03 -0400

    Generated report completion

--- TreePLE-Web/ListAllTrees/src/components/Report.vue
+++ TreePLE-Web/ListAllTrees/src/components/Report.vue
@@ -12,6 +12,39 @@
       <tr></tr>
   </table>
   <hr>
-    </div>
+  <h2>Sustainability Report</h2>
+  <br>
+  <h5>Basic information</h5>
+  <table align="center">
+      <tr>
+        <td style="padding:0 5px 0 5px;">Location Perimeter</td>
+        <td style="padding:0 5px 0 5px;">TO DO 98.333, 34.555, 98.4444, 34.5666 TO DO</td>
+      </tr>
+      <tr>
+        <td style="padding:0 5px 0 5px;">Reporter</td>
+        <td style="padding:0 5px 0 5px;">TO DO John Doe TO DO</td>
+      </tr>
+      <tr>
+        <td style="padding:0 5px 0 5px;">Date</td>
+        <td style="padding:0 5px 0 5px;">TO DO January 1st, 2018 TO DO</td>
+      </tr>
+  </table>
+  <br>
+  <h5>Computed values</h5>
+  <table align="center">
+      <tr>
+        <td style="padding:0 5px 0 5px;">Biological Index</td>
+        <td style="padding:0 5px 0 5px;">TO DO 6.0 TO DO</td>
+      </tr>
+      <tr>
+        <td style="padding:0 5px 0 5px;">Canopy Size</td>
+        <td style="padding:0 5px 0 5px;">TO DO 5m TO DO</td>
+      </tr>
+      <tr>
+        <td style="padding:0 5px 0 5px;">Carbon Sequestration Index</td>
+        <td style="padding:0 5px 0 5px;">TO DO 4.0 TO DO</td>
+      </tr>
+  </table>
+ </div>
 </template>
 
