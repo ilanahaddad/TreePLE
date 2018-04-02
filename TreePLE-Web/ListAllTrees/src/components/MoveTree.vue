29c794c70f7b50c5074d695d285f3741aa1001f2
Diana Serra <dianaiserra07@gmail.com>
2018-04-02 15:45:11 -0400

    Succesfully formated move tree form in web front end

---
+++ TreePLE-Web/ListAllTrees/src/components/MoveTree.vue
@@ -0,0 +1,37 @@
+<style>
+tr,td {
+    padding: 15px;
+}
+</style>
+<template>
+  <div id="moveTree">
+    <h2>Edit Tree</h2>
+    <table align="center">
+      <tr>
+    		<td>
+					Tree ID:
+				</td>
+				<td>
+					<input type="text" v-model= "treeId" placeHolder= "Tree ID">
+				</td>
+    		<td>
+					New Latitude:
+				</td>
+				<td>
+					<input type="text" v-model= "newLat" placeHolder= "Latitude">
+				</td>
+				<td>
+					New Longitude:
+				</td>
+				<td>
+					<input type="text" v-model= "newLong" placeHolder= "Longitude">
+				</td>
+			</tr>
+    </table>
+ 		<button align="center">Move Tree</button>
+  </div>
+</template>
+
+<script src="./moveTree.js">
+</script>
+
---
+++ TreePLE-Web/ListAllTrees/src/components/moveTree.js
@@ -0,0 +1,17 @@
+//import axios from 'axios'
+var config = require('../../config')
+
+export default {
+  name: 'MoveTree',
+	//add comma
+  data () {
+    return {
+      treeID: '',
+      newLat: '',
+      newLong: ''
+    }
+  } 
+}
+
+
+
