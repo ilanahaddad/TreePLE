102855dc2ee448213f4c5a0e682267457d5c597a
Thomas Hannaford <thomas.hannaford@mail.mcgill.ca>
2018-04-02 18:41:03 -0400

    minor changes before merge

--- TreePLE-Web/ListAllTrees/src/components/EditTree.vue
+++ TreePLE-Web/ListAllTrees/src/components/EditTree.vue
@@ -12,6 +12,72 @@
       <tr></tr>
   </table>
   <hr>
+  <h2>Edit Tree</h2>
+    <table align="center">
+      <tr>
+        <td>
+          Tree ID:
+        </td>
+        <td>
+          <input type="text" v-model= "treeId" placeHolder= "Tree ID">
+        </td>
+        <td>
+          Owner:
+        </td>
+        <td>
+          <input type="text" v-model="ownerName" placeHolder="Name">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Species Name:
+        </td>
+        <td>
+          <input type="text" v-model="species" placeHolder="Species">
+        </td>
+        <td>
+          Age: 
+        </td>
+        <td>
+          <input type="text" v-model="age" placeHolder="Age (yrs)">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Diameter:
+        </td>
+        <td>
+          <input type="text" v-model="diameter" placeHolder="Diameter (m)">
+        </td>
+        <td>
+          Height:
+        </td>
+        <td>
+          <input type="text" v-model="height" placeHolder="Height (m)">
+        </td> 
+      </tr>
+      <tr>
+        <td>
+          Municipality:
+        </td>
+        <td>
+          <input type="text" v-model="municipality" placeHolder="Municipality">
+        </td>
+        <td>
+          Land Use:
+        </td>
+        <td>
+          <input type="text" v-model="landuse" placeHolder="Land Use">
+        </td>
+      </tr>
+    </table>
+    <button align="center">Edit Tree Data</button>
  </div>
 </template>
+
+<style>
+tr,td {
+    padding: 15px;
+}
+</style>
 
--- TreePLE-Web/ListAllTrees/src/components/MoveTree.vue
+++ TreePLE-Web/ListAllTrees/src/components/MoveTree.vue
@@ -13,6 +13,34 @@
   </table>
   <hr>
   <h2>Move Tree</h2>
+    <table align="center">
+      <tr>
+        <td>
+          Tree ID:
+        </td>
+        <td>
+          <input type="text" v-model= "treeId" placeHolder= "Tree ID">
+        </td>
+        <td>
+          New Latitude:
+        </td>
+        <td>
+          <input type="text" v-model= "newLat" placeHolder= "Latitude">
+        </td>
+        <td>
+          New Longitude:
+        </td>
+        <td>
+          <input type="text" v-model= "newLong" placeHolder= "Longitude">
+        </td>
+      </tr>
+    </table>
+    <button align="center">Move Tree</button>
  </div>
 </template>
+
+<style>
+tr,td {
+    padding: 15px;
+}
 
