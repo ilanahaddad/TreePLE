c6f7ff7ecb2872efa8b506f615def66349e372f8
Diana Serra <dianaiserra07@gmail.com>
2018-04-02 18:29:36 -0400

    erased unecessary code

--- TreePLE-Web/ListAllTrees/src/components/treelist.js
+++ TreePLE-Web/ListAllTrees/src/components/treelist.js
@@ -8,20 +8,6 @@ var AXIOS = axios.create({
   baseURL: backendUrl,
   headers: { 'Access-Control-Allow-Origin': frontendUrl }
 })
-/*
-var acc = document.getElementsByClassName("accordion");
-var i;
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}*/
 
 function TreeDto (species, height, diameter, coordinates, owner, treeMunicipality, versions, land, status, id) {
   this.id = id
