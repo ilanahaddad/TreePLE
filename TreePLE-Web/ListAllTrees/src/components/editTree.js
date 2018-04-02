6f4942799b4685a2017995395bfda613c6fe1016
Diana Serra <diana.serra@mail.mcgill.ca>
2018-04-02 01:17:05 -0400

    Added and completed files for editing tree data

---
+++ TreePLE-Web/ListAllTrees/src/components/editTree.js
@@ -0,0 +1,22 @@
+//import axios from 'axios'
+var config = require('../../config')
+
+export default {
+  name: 'EditTree',
+	//add comma
+  data () {
+    return {
+      treeId: '',
+      ownerName: '',
+      species: '',
+      age: '',
+			diameter: '',
+			height: '',
+			municipality: '',
+			landuse: ''
+    }
+  }  
+}
+
+
+
