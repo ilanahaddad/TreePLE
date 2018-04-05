f3659f48c32766f09924eed2bdef7b4e59983d14
ilana-haddad <ilana.haddad@mail.mcgill.ca>
2018-04-05 19:54:33 -0400

    changed ports and ips to test on server

--- TreePLE/src/main/java/ca/mcgill/ecse321/TreePLE/persistence/PersistenceXStream.java
+++ TreePLE/src/main/java/ca/mcgill/ecse321/TreePLE/persistence/PersistenceXStream.java
@@ -23,10 +23,9 @@ import ca.mcgill.ecse321.TreePLE.model.VersionManager;
 // The second is the key type that is used to look it up. This example will not use it.
 @Repository
 public class PersistenceXStream {
-//TODO: ADD SURVEY 
 	private static XStream xstream = new XStream();
-	//private static String filename = "/webservice/data.xml";
-	private static String filename = "data.xml";
+	private static String filename = "/webservice/data.xml";
+	//private static String filename = "data.xml";
 	public static VersionManager initializeModelManager(String fileName) {
 		// Initialization for persistence
 		VersionManager vm;
