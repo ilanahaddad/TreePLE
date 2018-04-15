64814c9d8b130dac2c9c31b3f0f078624eba0524
Thomas Hannaford <thomas.hannaford@mail.mcgill.ca>
2018-04-15 00:39:02 -0400

    comment out refresh apge

--- TreePLE-Web/ListAllTrees/src/components/treelist.js
+++ TreePLE-Web/ListAllTrees/src/components/treelist.js
@@ -294,7 +294,7 @@ export default {
         .then(response => {
           // JSON responses are automatically parsed.
          	this.selectedVersion=''
-          window.location.reload();
+          //window.location.reload();
         })
         .catch(e => {
           this.errorLandUse = e;
