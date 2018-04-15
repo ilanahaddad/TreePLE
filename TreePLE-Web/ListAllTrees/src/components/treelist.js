97ff386d256bf6990c3b2a93e76eae7a145d1b5b
Thomas Hannaford <thomas.hannaford@mail.mcgill.ca>
2018-04-15 00:27:48 -0400

    automatic refresh page

--- TreePLE-Web/ListAllTrees/src/components/treelist.js
+++ TreePLE-Web/ListAllTrees/src/components/treelist.js
@@ -294,6 +294,7 @@ export default {
         .then(response => {
           // JSON responses are automatically parsed.
          	this.selectedVersion=''
+          window.location.reload();
         })
         .catch(e => {
           this.errorLandUse = e;
